775a4fd1584b074be4db083d04db6b68755eef00
J. Fronny <github@akbvopenfl1.hopto.org>
2025-01-05 10:47:39 +0000

    Fix actions push

--- src/helpers/workflows.ts
+++ src/helpers/workflows.ts
@@ -52,8 +52,8 @@ const publishPage = async(config: UpptimeConfig) => {
         name: Upload Pages artifact
         with:
           path: "site/status-page/__sapper__/export/"
-      - uses: Deploy to GitHub Pages
-        uses: actions/deploy-pages@v4
+      - uses: actions/deploy-pages@v4
+        name: Deploy to GitHub Pages
     `
   } else {
     return `
