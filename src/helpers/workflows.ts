5c60d8fbd840a19483bf6ce12b40040fa6b3d826
J. Fronny <github@akbvopenfl1.hopto.org>
2025-01-05 10:35:59 +0000

    Mention forked source

--- src/helpers/workflows.ts
+++ src/helpers/workflows.ts
@@ -36,7 +36,7 @@ const introComment = async () => `#
 # 🔼 Upptime @${await getUptimeMonitorVersion()}
 # GitHub-powered open-source uptime monitor and status page by Anand Chowdhary
 
-# * Source: https://github.com/upptime/upptime
+# * Source: https://github.com/JFronny/uptime-monitor
 # * Docs and more: https://upptime.js.org
 # * More by Anand Chowdhary: https://anandchowdhary.com
 `;
